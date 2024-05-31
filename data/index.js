72a80f5ca5ad1faaa4954607ba307663726f275c
j0rbe3n <jero.guzman@gmail.com>
2024-05-31 10:48:53 -0700

    NB

--- data/index.js
+++ data/index.js
@@ -61,7 +61,7 @@ import videoBG from "@images/video.png";
 export const VIDEO_DATA = {
   title: "Así se forman las Redes de Contribución ABANTU",
   image: videoBG,
-  videoID: "Kl5B6MBAntI"
+  videoID: "u2KXu6RQ2Ys"
 };
 
 import appFeature1 from "@images/ubuntu.jpg";
